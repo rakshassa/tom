f85e013d62020b1ca65b0cbed13e209cdf2098d3
Elijah Hall <rakshassa@gmail.com>
2014-03-03 22:17:00 -0600

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tom::Application.config.secret_key_base = 'ad130701d07b635dc9f21413a87fe9578f299104ab293b83564e2dde3cef9a2edd7fa560136b55698af7ce6ad1e096ac825eb3150615fe96d8484a3d005b58ea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tom::Application.config.session_store :cookie_store, key: '_tom_session'
